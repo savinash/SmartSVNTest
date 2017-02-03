bf19c4c2d65a34fab287c9a61a9e0fdbfa8759d7
Avinash <avinash@Avinashs-MacBook-Air.local>
2017-02-03 16:48:02 +0530

    same line

--- SwiftApp/ViewController.swift
+++ SwiftApp/ViewController.swift
@@ -18,7 +18,7 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        print("Avinash")
+        print("Avinash New print statement")
         
         print("New Print Statement")
         // Do any additional setup after loading the view, typically from a nib.
